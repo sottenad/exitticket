55f7f297011f3d0c30595082946aaa46129e1b6f
Steve Ottenad <sottenad@gmail.com>
2015-02-06 20:42:57 -0800

    sendgrid again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,16 +3,13 @@ Rails.application.configure do
 
     config.action_mailer.default_url_options = { :host => 'exitticket.herokuapp.com' }
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.raise_delivery_errors = true
-
-
     ActionMailer::Base.smtp_settings = {
         :address        => "smtp.sendgrid.net",
         :port           => "587",
         :authentication => :plain,
         :user_name      => ENV['SENDGRID_USERNAME'],
         :password       => ENV['SENDGRID_PASSWORD'],
-        :domain         => 'heroku.com'
+        :domain         => 'exitticket.herokuapp.com'
     }
     
   # Code is not reloaded between requests.
